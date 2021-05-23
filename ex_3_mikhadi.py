53deba68d270613a20d01086eda58982497bb880
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2021-05-23 18:24:18 +0300

    input added

--- ex_3_mikhadi.py
+++ ex_3_mikhadi.py
@@ -1,2 +1,10 @@
-print("something")
-print()
+import sympy as sp
+
+x = sp.symbols('x')
+func = 0
+deg = int(input("Which degree of function?: "))
+print("Enter function")
+for i in range(deg, -1, -1):
+    t = float(input(f"(x**{i})*"))
+    func += t*x**i
+print(f"Your func: {func}")
